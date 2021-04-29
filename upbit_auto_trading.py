7b0eab497ed3234839bf75d83bf6111013afdb58
github923 <78134092+github923@users.noreply.github.com>
2021-04-30 00:05:42 +0900

    Update upbit_auto_trading.py

--- upbit_auto_trading.py
+++ upbit_auto_trading.py
@@ -6,6 +6,10 @@ access = "jXmymUVzgyJnixtTcYPfP7axp5YKpceWqBxVBplY"
 secret = "Pc1YbwzPgJxVzwPYF9fPkenfqt3pKi50geoxLruq"
 
 
+# 로그인
+upbit = pyupbit.Upbit(access, secret)
+# print("autotrade start")
+
 def get_balance(ticker):
     """잔고 조회"""
     balances = upbit.get_balances()
@@ -20,10 +24,6 @@ def get_current_price(ticker):
     """현재가 조회"""
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
-# 로그인
-upbit = pyupbit.Upbit(access, secret)
-print("autotrade start")
-
 
 def macd_osc(ticker):
     df = pyupbit.get_ohlcv(ticker, interval='minute1', count = '200')
@@ -37,35 +37,34 @@ def trading(ticker):
     df = macd_osc(ticker)
     df['macdo_is_positive'] = df['macdo'] > 0
     
-    if (df.iloc[-2]['macdo_is_positive'] == 1):
+    if (df.iloc[-1]['macdo_is_positive'] == 1):
         buy_or_sell = 1
-    elif (df.iloc[-2]['macdo_is_positive'] == 0):
+    elif (df.iloc[-1]['macdo_is_positive'] == 0):
         buy_or_sell = 2
     else :
         buy_or_sell = 0
 
     return buy_or_sell
 
-
-
-# 자동매매 시작
 while True:
     try:
+        a = time.time()
         if trading('KRW-DOGE') == 1:
             krw = get_balance("KRW")
             upbit.buy_market_order("KRW-DOGE", krw*0.9995)
             print("buy")
         elif trading('KRW-DOGE') == 2:
-            coin = get_balance("KRW-DOGE")
+            coin = upbit.get_balance('KRW-DOGE')
             upbit.sell_market_order("KRW-DOGE", coin*0.9995)
             print("sell")
         elif trading('KRW-DOGE') == 0:
             print('no')
         else :
             pass
-        print(macd_osc('KRW-DOGE').iloc[-1]['macdo'])
-        time.sleep(1)
-        
+        print('macdo',macd_osc('KRW-DOGE').iloc[-1]['macdo'])
+        b = time.time()
+        print('time:',b-a)
+        time.sleep(7.5)
     except Exception as e:
         print(e)
         time.sleep(1)
