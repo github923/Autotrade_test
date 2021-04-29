77f9b29f597f4fc52b5271eb3fbfaa522aae0338
github923 <78134092+github923@users.noreply.github.com>
2021-04-29 11:11:18 +0900

    Update upbit_auto_trading.py

--- upbit_auto_trading.py
+++ upbit_auto_trading.py
@@ -5,38 +5,6 @@ import datetime
 access = "jXmymUVzgyJnixtTcYPfP7axp5YKpceWqBxVBplY"
 secret = "Pc1YbwzPgJxVzwPYF9fPkenfqt3pKi50geoxLruq"
 
-def macd_osc_trading(ticker):
-    df = pyupbit.get_ohlcv(ticker, interval='minute30', count = '200')
-    macd = df.close.ewm(span=12).mean() - df.close.ewm(span=26).mean() # 장기(26) EMA
-    macds = macd.ewm(span=9).mean() # Signal
-    macdo = macd - macds # Oscillato
-    df['macdo'] = macdo
-    df['macdo_is_positive'] = df['macdo'] > 0
-    buy_or_sell = []
-    for x in range(200):
-        if x >= 1:
-            if (df.iloc[x-1]['macdo_is_positive'] == 0) and (df.iloc[x]['macdo_is_positive'] == 0):
-                buy_or_sell.append('no')
-            elif (df.iloc[x-1]['macdo_is_positive'] == 1) and (df.iloc[x]['macdo_is_positive'] == 1):
-                buy_or_sell.append('no')      
-            elif (df.iloc[x-1]['macdo_is_positive'] == 0) and (df.iloc[x]['macdo_is_positive'] == 1):
-                buy_or_sell.append('buy')     
-            elif (df.iloc[x-1]['macdo_is_positive'] == 1) and (df.iloc[x]['macdo_is_positive'] == 0):
-                buy_or_sell.append('sell')     
-            else :
-                pass
-        else:
-            buy_or_sell.append('first')
-    df['buy_or_sell'] = buy_or_sell
-    
-    return df['buy_or_sell'][-1]
-
-
-def get_start_time(ticker):
-    """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute30", count=1)
-    start_time = df.index[0]
-    return start_time
 
 def get_balance(ticker):
     """잔고 조회"""
@@ -56,21 +24,52 @@ def get_current_price(ticker):
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
+
+def macd_osc(ticker):
+    df = pyupbit.get_ohlcv(ticker, interval='minute30', count = '200')
+    macd = df.close.ewm(span=12).mean() - df.close.ewm(span=26).mean()
+    macds = macd.ewm(span=9).mean()
+    macdo = macd - macds
+    df['macdo'] = macdo
+    return df
+
+def trading(ticker):
+    df = macd_osc(ticker)
+    df['macdo_is_positive'] = df['macdo'] > 0
+    
+    if (df.iloc[-2]['macdo_is_positive'] == 0) and (df.iloc[-1]['macdo_is_positive'] == 0):
+        buy_or_sell = 0
+    elif (df.iloc[-2]['macdo_is_positive'] == 1) and (df.iloc[-1]['macdo_is_positive'] == 1):
+        buy_or_sell = 0
+    elif (df.iloc[-2]['macdo_is_positive'] == 0) and (df.iloc[-1]['macdo_is_positive'] == 1):
+        buy_or_sell = 1
+    elif (df.iloc[-2]['macdo_is_positive'] == 1) and (df.iloc[-1]['macdo_is_positive'] == 0):
+        buy_or_sell = 2
+    else :
+        pass
+
+    return buy_or_sell
+
+
+
 # 자동매매 시작
 while True:
-    coin_sell_price = 5000/get_current_price('KRW-AXS')
     try:
-        if macd_osc_trading(ticker = 'KRW-AXS') == 'buy':
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-AXS", krw*0.9995)
-        elif macd_osc_trading(ticker = 'KRW-AXS') == 'sell':
-            coin = get_balance("KRW-AXS")
-            if coin > coin_sell_price:
-                upbit.sell_market_order("KRW-AXS", coin*0.9995)
-        # print(macd_osc_trading(ticker = 'KRW-AXS'))
+        if trading('KRW-DOGE') == 1:
+            krw = get_balance("KRW")
+            upbit.buy_market_order("KRW-DOGE", krw*0.9995)
+            print("buy")
+        elif trading('KRW-DOGE') == 2:
+            coin = get_balance("KRW-DOGE")
+            upbit.sell_market_order("KRW-DOGE", coin*0.9995)
+            print("sell")
+        elif trading('KRW-DOGE') == 0:
+            print('no')
+        else :
+            pass
+        print(macd_osc('KRW-DOGE').iloc[-1]['macdo'])
         time.sleep(1)
+        
     except Exception as e:
         print(e)
         time.sleep(1)
